1083d00d309d8234ddd4e4d95928dcb1b76dec63
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-02-27 16:53:30 +0530

    configuring github-actions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,9 +8,6 @@ const Layout = (props) => {
 
     return( 
             <div >
-                {/* <div class="bg"></div>
-                <div class="bg bg2"></div>
-                <div class="bg bg3"></div> */}
                 <Header/>
                 <div className="content-main">
                  {props.children}
