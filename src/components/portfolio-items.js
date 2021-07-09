97807d0da1620d82d1c5073c8c670c8afb17b672
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-07-10 00:19:17 +0530

    fixed the resume bug

--- src/components/portfolio-items.js
+++ src/components/portfolio-items.js
@@ -1,7 +1,7 @@
 const portfolioItems = [
     {
         title: 'Lount',
-        sinopsis: 'A fullstack chat application built with graphql and reactjs.',
+        sinopsis: 'A fullstack chat application built with graphql and reactjs. Click to open',
         desc: 'This is one of the most exciting projects I have built. It\'s a chat application. Backend is built with Graphql, Apollo, postgreSQl and Prisma ORM. Front end is bult with Reactjs and ChakraUI. The app is deployed on AWS EC2 and uses graphql subscriptions for sending/receiveing messages and you can react to them with emojis in real time.',
         img: './portfolio/lountchat.png',
         repo: 'https://github.com/jarryingnut/lount',
@@ -11,7 +11,7 @@ const portfolioItems = [
     
     {
         title: 'Gigapost',
-        sinopsis: 'A Social Blogging platform built with MERN Stack. Click to open',
+        sinopsis: 'A Social Blogging platform built with MERN Stack. ',
         desc: 'Gigapost is a full-stack blogging application and is built with MERN stack. Tests are written in jest and cypress. The application allows you to register as a new user or login into existing account. You can create, update, delete, comment, like other people\'s posts. I have used Redux for managing state. UI is built with the help of chakraUI and the whole application is deployed on heroku.',
         img: './portfolio/gigapost.png',
         repo: 'https://github.com/jarryingnut/gigapost',
